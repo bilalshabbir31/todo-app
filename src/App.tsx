c86ec51156bd46416c8c3a1df6f456b5b371bdb3
Muhammad Bilal <bilaltheofficial@gmail.com>
2023-04-01 14:07:57 +0500

    pass childrens to props

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,29 @@
-import ListGroup from "./components/ListGroup";
+import { Alert } from "./components/Alert";
 
 
 function App() {
 
-  let items=[
-    'Mian Channu',
-    'Lahore',
-    'Okara',
-    'Sahiwal'
-];
+//   let items=[
+//     'Mian Channu',
+//     'Lahore',
+//     'Okara',
+//     'Sahiwal'
+// ];
 
-const handleSelectItem=(item: string)=>{
-  console.log(item);
-}
+// const handleSelectItem=(item: string)=>{
+//   console.log(item);
+// }
 
-  return <div><ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem}/></div>
+  // return <div><ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem}/></div>
+  return (
+    <div>
+      <Alert>
+        Hello
+        <span>World</span>
+        <h1>Bilal</h1>
+      </Alert>
+    </div>
+  );
 }
 
 
