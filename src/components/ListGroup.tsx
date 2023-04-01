7e2a650bcaf9309a270500355a613b265a5099c2
Muhammad Bilal <bilaltheofficial@gmail.com>
2023-04-01 13:17:10 +0500

    add events

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,3 +1,4 @@
+import { MouseEvent } from "react";
 
 function ListGroup() {
 
@@ -8,14 +9,18 @@ function ListGroup() {
         'Sahiwal'
     ];
 
-    items=[]
+    // Event Handler
+    const handleClick=(event: MouseEvent)=> console.log(event);
 
     return (
         <>
         <h1>List</h1>
-        {items.length===0    && <p>No Item Found</p>}
+        {items.length===0 && <p>No Item Found</p>}
         <ul className="list-group">
-            {items.map(item=><li key={item}>{item}</li>)}
+            {items.map(item=>
+            <li className="list-group-item"
+             key={item} onClick={handleClick}>
+             {item}</li>)}
         </ul>
         </>
     );
